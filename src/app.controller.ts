997f58e478e86b257f9f1b24a0555770c6fb7c20
sukikujun <sukikujun@gmail.com>
2024-02-14 04:05:18 +0900

    chapter2 p5

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { readFileSync, readdirSync } from 'fs';
+import { resolve } from 'path';
+import _ from 'lodash';
+import { Response } from 'express';
+import { readFile, readdir } from 'fs/promises';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(@Res() res: Response) {
+    const files = await readdir(resolve(__dirname, '..', 'wallpaper'))
+    // return 'http://localhost:3000/wallpaper/' + files[_.random(files.length - 1)]
+
+    const file = await readFile(resolve(__dirname, '../wallpaper', files[_.random(files.length - 1)]))
+
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        // res.set('Content-type', 'image/jpeg')
+        res.type('image/jpeg')
+        return res.send(file)
+      }, 2000);
+    })
+
   }
 }
